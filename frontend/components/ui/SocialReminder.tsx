983f310239472f72c5cb0c7ad1976c4678ac9242
KhoiUna <thorwaitson@gmail.com>
2024-03-03 21:11:13 -0600

    modified UI:   components/ui/SocialReminder.tsx

--- frontend/components/ui/SocialReminder.tsx
+++ frontend/components/ui/SocialReminder.tsx
@@ -1,9 +1,30 @@
-export default function SocialReminder() {
-    return (
-        <div className="border-4 border-dotted border-red-500 p-3 max-w-sm bg-white">
-            <p><strong>THÔNG BÁO:</strong> Trang web chỉ giới thiệu sản phẩm, quý khách hàng vui lòng liên hệ <a href="https://www.facebook.com/5piecesclothing" target="_blank" rel="noreferrer" className="text-blue-500 underline underline-offset-4 font-bold">Facebook</a> hoặc <a href="https://www.instagram.com/5piecesclothing/" target="_blank" rel="noreferrer" className="text-blue-500 underline underline-offset-4 font-bold">Instargram</a> để tiến hành thanh toán.</p>
+import { BRAND_SOCIALS } from "@/app/config";
 
-            <p className="pt-3"><strong>NOTICE:</strong> The website only introduces products, customers please contact <a href="https://www.facebook.com/5piecesclothing" target="_blank" rel="noreferrer" className="text-blue-500 underline underline-offset-4 font-bold">Facebook</a> or <a href="https://www.instagram.com/5piecesclothing/" target="_blank" rel="noreferrer" className="text-blue-500 underline underline-offset-4 font-bold">Instargram</a> to make payment.</p>
-        </div>
-    )
+export default function SocialReminder() {
+	return (
+		<div className="border-4 border-dotted border-red-500 p-3 max-w-sm bg-white">
+			<p className="pt-3">
+				<strong>NOTICE:</strong> The website only introduces products,
+				customers please contact{" "}
+				<a
+					href={BRAND_SOCIALS.facebook}
+					target="_blank"
+					rel="noreferrer"
+					className="text-blue-500 underline underline-offset-4 font-bold"
+				>
+					Facebook
+				</a>{" "}
+				or{" "}
+				<a
+					href={BRAND_SOCIALS.instagram}
+					target="_blank"
+					rel="noreferrer"
+					className="text-blue-500 underline underline-offset-4 font-bold"
+				>
+					Instargram
+				</a>{" "}
+				to make payment.
+			</p>
+		</div>
+	);
 }
